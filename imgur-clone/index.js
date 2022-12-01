e07fd0c5a43a18c6cbd637653f940374877119bc
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-12-02 01:02:11 +0530

    done

--- imgur-clone/index.js
+++ imgur-clone/index.js
@@ -1,14 +1,19 @@
-
+const Access_Key = `MnGU_LoI0DbC9Vl8-nuGqvBf3D35W9WloqUS-oh_0Hs`;
+const Secret_key = `PBm8d2pjtqmU3s18S3KYcMwaHmpaVKYLy_mCMsQp2YA`;
 
 let pageNO = 1;
 let checkappend = true;
 
-const getdata = ()=>{
-    if(pageNO*10 >= 5000){
+const getdata = (search)=>{
+    if(pageNO >= 8000){
         pageNO = 1;
     }
 
-    let url = `https://jsonplaceholder.typicode.com/photos?_page=${pageNO}&_limit=10`
+    let url = `https://api.unsplash.com/photos/?page=${pageNO}&per_page=10&client_id=${Access_Key}`;
+
+    if(search)
+    url = `https://api.unsplash.com/photos/?page=${pageNO}&per_page=10&client_id=${Access_Key}&query=${search}`;
+    
     
     fetch(url).then((res)=>res.json()).then((res)=>{
         appenddata(res);
@@ -21,7 +26,7 @@ const appenddata = (data)=>{
     data.map((elem)=>{
         let div = document.createElement("div");
         let img = document.createElement("img");
-        img.src = elem.url;
+        img.src = elem.urls.small;
         div.append(img);
         subparent.append(div);
     })
@@ -57,3 +62,58 @@ window.addEventListener('scroll', ()=>{
     }
 })
 
+
+
+
+var id;
+function debounce(){
+    clearTimeout(id);
+     id = setTimeout(function(){
+        getsearchdata();
+    }, 1000);
+
+}
+
+const getsearchdata = ()=>{
+    
+        let search = document.getElementById("searchinput").value;
+
+        let url = `https://api.unsplash.com/photos/?page=${pageNO}&per_page=20&client_id=${Access_Key}&query=${search}`
+        fetch(url).then((res)=>res.json()).then((data)=>{
+
+            if((data.errors)){
+                document.getElementById("databox").classList.remove("show");
+            }else{
+                document.getElementById("databox").classList.add("show");
+                console.log(data);
+                appendsearch(data);
+            }
+        })
+
+}
+
+
+
+const appendsearch = (data)=>{
+    let databox = document.getElementById("databox");
+    databox.innerHTML = null;
+
+    data.map((elem)=>{
+        let div = document.createElement("div");
+        div.setAttribute("class", "datadiv");
+        div.addEventListener("click", function(){
+            getdata(elem.alt_description);
+            databox.innerHTML = null;
+            document.getElementById("databox").classList.remove("show");
+        })
+        let para = document.createElement("p");
+        para.innerText = elem.alt_description;
+        div.append(para);
+        databox.append(div);
+    })
+}
+
+document.getElementById("parent").addEventListener("click",()=>{
+    document.getElementById("databox").innerHTML = null;
+    document.getElementById("databox").classList.remove("show");
+})
